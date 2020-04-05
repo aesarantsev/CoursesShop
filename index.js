e99489bccc89851e9ebe75d64f256bf84cf24f38
aesarantsev <andrrr96@yandex.ru>
2020-04-05 14:28:38 +1000

    feature(Cart) Add cart

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const homeRoutes = require("./routes/home");
 const cardRoutes = require("./routes/card");
 const addRoutes = require("./routes/add");
 const coursesRoutes = require("./routes/courses");
+const User = require("./models/user");
 
 const app = express();
 
@@ -17,6 +18,16 @@ const hbs = exphbs.create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "views");
+
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById("5e87cce0fbd5a4179c360852");
+    req.user = user;
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -37,6 +48,17 @@ async function start() {
       useNewUrlParser: true,
       useFindAndModify: false
     });
+
+    const candidate = await User.findOne();
+
+    if (!candidate) {
+      const user = new User({
+        email: "test@test.ru",
+        name: "test",
+        cart: { items: [] }
+      });
+      await user.save();
+    }
 
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
--- models/course.js
+++ models/course.js
@@ -11,7 +11,20 @@ const course = new Schema({
   },
   img: {
     type: String
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
   }
+});
+
+course.method("toClient", function() {
+  const course = this.toObject();
+
+  course.id = course._id;
+  delete course._id;
+
+  return course;
 });
 
 module.exports = model("Course", course);
